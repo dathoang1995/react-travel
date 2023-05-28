52bcc682a72d2f319d954f0657453badb0dd07f9
Your <you@example.com>
2023-05-28 15:53:14 +0700

    second commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,150 +1,150 @@
-// /* eslint-disable jsx-a11y/anchor-is-valid */
-// import React from "react";
-// import { FiSend, FiChevronRight } from "react-icons/fi";
-// import { MdOutlineTravelExplore } from "react-icons/md";
-// import { AiOutlineTwitter, AiFillYoutube, AiFillInstagram } from "react-icons/ai";
-// import { FaTripadvisor } from "react-icons/fa";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { FiSend, FiChevronRight } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiOutlineTwitter, AiFillYoutube, AiFillInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
 
-// import "./footer.scss";
-// import video2 from "../../assets/video(2).mp4";
+import "./footer.scss";
+import video2 from "../../assets/video(2).mp4";
 
-// function Footer() {
-//     return (
-//         <section className='footer'>
-//             <div className='videoDiv'>
-//                 <video src={video2} loop autoPlay muted typeof='video/mp4'></video>
-//             </div>
+function Footer() {
+    return (
+        <section className='footer'>
+            <div className='videoDiv'>
+                <video src={video2} loop autoPlay muted typeof='video/mp4'></video>
+            </div>
 
-//             <div className='secContent container'>
-//                 <div className='contactDiv flex'>
-//                     <div className='text'>
-//                         <small>KEEP IN TOUCH</small>
-//                         <h2>Travel with us</h2>
-//                     </div>
+            <div className='secContent container'>
+                <div className='contactDiv flex'>
+                    <div className='text'>
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
 
-//                     <div className='inputDiv flex'>
-//                         <input type='text' placeholder='Enter Email Address' />
-//                         <button className='btn flex' type='submit'>
-//                             SEND <FiSend className='icon' />
-//                         </button>
-//                     </div>
-//                 </div>
+                    <div className='inputDiv flex'>
+                        <input type='text' placeholder='Enter Email Address' />
+                        <button className='btn flex' type='submit'>
+                            SEND <FiSend className='icon' />
+                        </button>
+                    </div>
+                </div>
 
-//                 <div className='footerCard flex'>
-//                     <div className='footerIntro flex'>
-//                         <div className='logoDiv'>
-//                             <a href='#' className='logo flex'>
-//                                 <MdOutlineTravelExplore className='icon' /> Travel
-//                             </a>
-//                         </div>
+                <div className='footerCard flex'>
+                    <div className='footerIntro flex'>
+                        <div className='logoDiv'>
+                            <a href='#' className='logo flex'>
+                                <MdOutlineTravelExplore className='icon' /> Travel
+                            </a>
+                        </div>
 
-//                         <div className='footerParagraph'>
-//                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat voluptatem
-//                             expedita culpa eligendi dolorem, quidem mollitia commodi et veritatis tempore, reiciendis
-//                             nostrum odio rerum magni optio minus perferendis. Quibusdam?
-//                         </div>
+                        <div className='footerParagraph'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat voluptatem
+                            expedita culpa eligendi dolorem, quidem mollitia commodi et veritatis tempore, reiciendis
+                            nostrum odio rerum magni optio minus perferendis. Quibusdam?
+                        </div>
 
-//                         <div className='footerSocial flex'>
-//                             <AiOutlineTwitter className='icon' />
-//                             <AiFillYoutube className='icon' />
-//                             <AiFillInstagram className='icon' />
-//                             <FaTripadvisor className='icon' />
-//                         </div>
-//                     </div>
+                        <div className='footerSocial flex'>
+                            <AiOutlineTwitter className='icon' />
+                            <AiFillYoutube className='icon' />
+                            <AiFillInstagram className='icon' />
+                            <FaTripadvisor className='icon' />
+                        </div>
+                    </div>
 
-//                     <div className='footerLinks grid'>
-//                         {/* {Group one} */}
-//                         <div className='linkGroup'>
-//                             <span className='groupTitle'>OUR AGENCY</span>
+                    <div className='footerLinks grid'>
+                        {/* {Group one} */}
+                        <div className='linkGroup'>
+                            <span className='groupTitle'>OUR AGENCY</span>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Services
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Services
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Insurance
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Agency
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Tourism
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Insurance
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Agency
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Tourism
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Payment
-//                             </li>
-//                         </div>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Payment
+                            </li>
+                        </div>
 
-//                         {/* {Group two} */}
-//                         <div className='linkGroup'>
-//                             <span className='groupTitle'>PARTNERS</span>
+                        {/* {Group two} */}
+                        <div className='linkGroup'>
+                            <span className='groupTitle'>PARTNERS</span>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Bookings
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Bookings
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Rentcars
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 HostelWorld
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Trivago
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Rentcars
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                HostelWorld
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Trivago
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 TripAdvisor
-//                             </li>
-//                         </div>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                TripAdvisor
+                            </li>
+                        </div>
 
-//                         {/* {Group three} */}
-//                         <div className='linkGroup'>
-//                             <span className='groupTitle'>LAST MINUTE</span>
+                        {/* {Group three} */}
+                        <div className='linkGroup'>
+                            <span className='groupTitle'>LAST MINUTE</span>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 London
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                London
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 California
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Indonesia
-//                             </li>
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Europe
-//                             </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                California
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Indonesia
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Europe
+                            </li>
 
-//                             <li className='footerList flex'>
-//                                 <FiChevronRight className='icon' />
-//                                 Oceania
-//                             </li>
-//                         </div>
-//                     </div>
+                            <li className='footerList flex'>
+                                <FiChevronRight className='icon' />
+                                Oceania
+                            </li>
+                        </div>
+                    </div>
 
-//                     <div className='footerDiv flex'>
-//                         <small>BEST TRAVEL WEBSITE THEME</small>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
+                    <div className='footerDiv flex'>
+                        <small>BEST TRAVEL WEBSITE THEME</small>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
 
-// export default Footer;
+export default Footer;
